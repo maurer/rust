1bb21aec403e74fa5b4e19ebdb5bc16c89fa0af3
Matthew Maurer <mmaurer@google.com>
2024-02-19 19:46:01 +0000

    Fix cfi dylib test

--- tests/ui/sanitize/cfi-dylib-test.rs
+++ tests/ui/sanitize/cfi-dylib-test.rs
@@ -1,8 +1,8 @@
 // needs-sanitizer-cfi
-// compile-flags: -Clto -Zsanitizer=cfi -C codegen-units=1 -C opt-level=0
+// compile-flags: -Clinker-plugin-lto -C link-args=-fuse-ld=lld -Zsanitizer=cfi -C codegen-units=1 -C opt-level=0 --test
 // run-pass
 
 #[test]
 fn foo() {
-    std::fs::File::open("boom").expect("baboom");
+    std::fs::File::open("boom").expect_err("baboom");
 }
